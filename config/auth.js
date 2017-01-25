0a0dfe900778451439831704eacb78bc5de72cf4
OmarWagih1 <o.wagih.ow@gmail.com>
2017-01-25 23:47:37 +0200

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports=
     twitterAuth: {
     consumerKey: process.env.consumerKey,
     consumerSecret: process.env.consumerSecret,
-    callbackURL: 'http://ojh-omarwagih.c9users.io:8080/auth/twitter/callback',
+    callbackURL: 'https://freecodevoteapp.herokuapp.com/auth/twitter/callback',
   }
 };
